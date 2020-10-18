816f643865b8a581aff2b2ef400c4e872877be06
Anthonyhelka <helkaanthony1997@gmail.com>
2020-10-17 22:35:33 -0400

    First Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform _player;
+    private float _mouseX, _mouseY, _xRotation;
+    [SerializeField] private float _mouseSensitivity = 50.0f;
+
+    private void Start()
+    {
+        _player = GameObject.Find("Player").GetComponent<Transform>();
+        if (_player == null) Debug.LogError("Player is NULL");
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        GetInput();
+        _xRotation -= _mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -60.0f, 60.0f);
+        transform.localRotation = Quaternion.Euler(_xRotation, 0.0f, 0.0f);
+        _player.Rotate(0.0f, _mouseX * _mouseSensitivity * Time.deltaTime, 0.0f);
+    }
+
+    private void GetInput()
+    {
+        _mouseX = Input.GetAxisRaw("Mouse X");
+        _mouseY = Input.GetAxisRaw("Mouse Y");
+    }
+}
---
+++ Assets/Scripts/FlashlightController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightController : MonoBehaviour
+{
+    public GameObject _light;
+    private bool _status;
+    private bool _mouseInput, _tInput;
+
+    void Start()
+    {
+        _light = gameObject.transform.Find("Light").gameObject;
+        if (_light == null) Debug.LogError("Light is NULL");
+    }
+
+    void Update()
+    {
+        GetInput();
+        if (_mouseInput || _tInput)
+        {
+            _status = !_status;
+            _light.SetActive(_status);
+        }
+    }
+
+    void GetInput()
+    {
+         _mouseInput = Input.GetKeyDown(KeyCode.Mouse0);
+         _tInput = Input.GetKeyDown(KeyCode.T);
+    }
+}
