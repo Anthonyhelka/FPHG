534edb70c54edd56d4252ebfa52ee9ae604926e4
Anthonyhelka <helkaanthony1997@gmail.com>
2020-10-18 13:15:13 -0400

    Added zombie enemy. Fixed chasing. Added animations for enemy.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,14 +8,18 @@ public class Enemy : MonoBehaviour
     private NavMeshAgent _navMeshAgent;
     private Transform _player;
     [SerializeField] private float _enemyChaseDistance = 7.0f;
-    [SerializeField] private float _speed = 3.5f;
+    [SerializeField] private float _speed = 4.0f;
+    [SerializeField] private float _acceleration = 10.0f;
     [SerializeField] private float _lookSpeed = 8.0f;
+    private Animator _animator;
 
     void Awake()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
         _player = GameObject.Find("Player").GetComponent<Transform>();
         _navMeshAgent.speed = _speed;
+        _animator = gameObject.transform.Find("Zombie").GetComponent<Animator>();
+        if (_animator == null) Debug.LogError("Zombie Animator is NULL");
     }
 
     void Update()
@@ -24,9 +28,15 @@ public class Enemy : MonoBehaviour
 
         if (distance < _enemyChaseDistance)
         {
+            _animator.SetBool("chasing", true);
+            _navMeshAgent.isStopped = false;
             Quaternion targetRotation = Quaternion.LookRotation(_player.transform.position - transform.position);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _lookSpeed * Time.deltaTime);
             _navMeshAgent.SetDestination(_player.transform.position);
+            _navMeshAgent.acceleration = _acceleration;
+        } else {
+            _navMeshAgent.isStopped = true;
+            _animator.SetBool("chasing", false);
         }
     }
 
